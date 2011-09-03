d46974010a004bc07be57527710baacdd35109d2
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2011-09-03 19:42:03 -0300

    style changes

--- blohg/mercurial_content.py
+++ blohg/mercurial_content.py
@@ -108,14 +108,15 @@ def setup_mercurial(app):
         # If we still don't want a refresh, it seems that we have an app.hg
         # object, then we need to check if it's new enough
         if not refresh:
-            # using the head of default branch revision, as we're not debugging, and just
-            # want the committed stuff.
-            if repo[repo.branchtags()["default"]].rev() != app.hg.repo[app.hg.repo.branchtags()["default"]].rev():
+            # using the head of default branch revision, as we're not
+            # debugging, and just want the committed stuff.
+            if repo[repo.branchtags()['default']].rev() \
+               != app.hg.repo[app.hg.repo.branchtags()['default']].rev():
                 refresh = True
 
         revision_id = None
         if not app.debug:
-            revision_id = repo.branchtags()["default"]
+            revision_id = repo.branchtags()['default']
 
         if g is not None:
             g.repo = repo[revision_id]
