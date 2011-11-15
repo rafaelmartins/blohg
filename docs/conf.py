fd022f0709ebaf8674d33f5f626e69c68a342785
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2011-11-15 03:48:49 -0200

    we're not using intersphinx at all.

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ import os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -222,6 +222,6 @@ man_pages = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {}
 
 highlight_language = 'none'
