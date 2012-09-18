dfae945838fcc632fc71f6e51da288d18716473c
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2012-09-18 02:57:32 -0300

    revert 5a55dec5565c

--- blohg/tests/__init__.py
+++ blohg/tests/__init__.py
@@ -11,8 +11,6 @@
 
 import unittest
 from blohg.tests.hgapi import HgApiTestCase
-from blohg.tests.hgapi.repo import RepositoryTestCase, \
-     RepoStateStableTestCase, RepoStateVariableTestCase
 from blohg.tests.hgapi.templates import MercurialLoaderTestCase
 
 
@@ -20,7 +18,4 @@ def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(HgApiTestCase))
     suite.addTest(unittest.makeSuite(MercurialLoaderTestCase))
-    suite.addTest(unittest.makeSuite(RepoStateStableTestCase))
-    suite.addTest(unittest.makeSuite(RepoStateVariableTestCase))
-    suite.addTest(unittest.makeSuite(RepositoryTestCase))
     return suite
