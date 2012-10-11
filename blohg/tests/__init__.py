22d08aaabd0b9e7e9774182738fdcc716245254a
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2012-10-11 02:07:50 -0300

    removed old hgapi, everything was reimplemented

--- blohg/tests/__init__.py
+++ blohg/tests/__init__.py
@@ -18,32 +18,19 @@ from blohg.tests.hg.changectx import ChangeCtxDefaultTestCase, \
 from blohg.tests.hg.filectx import FileCtxTestCase
 from blohg.tests.models import HgTestCase, PageTestCase, PostTestCase
 from blohg.tests.templating import MercurialLoaderTestCase
-
-#from blohg.tests.hgapi import HgApiTestCase
-#from blohg.tests.hgapi.models import PageTestCase, PostTestCase
-#from blohg.tests.hgapi.repo import RepositoryTestCase, \
-#     RepoStateStableTestCase, RepoStateVariableTestCase
-#from blohg.tests.hgapi.templates import MercurialLoaderTestCase
 from blohg.tests.utils import UtilsTestCase
 
 
 def suite():
     suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(AppTestCase))
     suite.addTest(unittest.makeSuite(HgRepositoryTestCase))
     suite.addTest(unittest.makeSuite(ChangeCtxDefaultTestCase))
     suite.addTest(unittest.makeSuite(ChangeCtxWorkingDirTestCase))
     suite.addTest(unittest.makeSuite(FileCtxTestCase))
+    suite.addTest(unittest.makeSuite(HgTestCase))
     suite.addTest(unittest.makeSuite(PageTestCase))
     suite.addTest(unittest.makeSuite(PostTestCase))
-    suite.addTest(unittest.makeSuite(HgTestCase))
-    suite.addTest(unittest.makeSuite(AppTestCase))
     suite.addTest(unittest.makeSuite(MercurialLoaderTestCase))
-
-    #suite.addTest(unittest.makeSuite(HgApiTestCase))
     suite.addTest(unittest.makeSuite(UtilsTestCase))
-    #suite.addTest(unittest.makeSuite(MercurialLoaderTestCase))
-    #suite.addTest(unittest.makeSuite(RepoStateStableTestCase))
-    #suite.addTest(unittest.makeSuite(RepoStateVariableTestCase))
-    #suite.addTest(unittest.makeSuite(RepositoryTestCase))
-
     return suite
