5a4685427394cb05b90dae7b221015811607382a
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2013-02-02 20:44:02 -0200

    added missing docstrings

---
+++ blohg/tests/vcs_backends/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    blohg.tests.vcs_backends
+    ~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Package with tests for blohg integration with VCS's.
+
+    :copyright: (c) 2010-2013 by Rafael Goncalves Martins
+    :license: GPL-2, see LICENSE for more details.
+"""
